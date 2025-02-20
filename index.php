8400fb04998b94800e98c03bdd0f1ba9acbe6d05
chloeluk852 <78545300+chloeluk852@users.noreply.github.com>
2025-02-20 18:23:47 +0800

    first commit

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Video List</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
       href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
       rel="stylesheet"
     />
+    <title>Video List</title>
     <style>
         body {
             font-family: "Montserrat", sans-serif;
@@ -31,22 +31,37 @@
     </style>
 </head>
 <body>
-    <h1>Creative Media Design Student Movie Trailers</h1>
+    <h1>Video List</h1>
     <ul>
     <?php
     $videos = array(
-        "Abby Yu.mp4", "Addison Ong.mp4", "Adelle Wu.mp4", "Aison Chiu.mp4",
-        "Andreea Cheung.mp4", "Annette Yu.mp4", "Brandon Kwok.mp4", "Charles Liu.mp4",
-        "Chloe Wong.mp4", "Curtis Leung.mp4", "Curtis Wong.mp4", "Cyrus Lai.mp4",
-        "Elgar Lam.mp4", "Gwan Yu Raven Siu.mov", "Hei Yu Hailey Siu.mp4",
-        "Jayden Heng.mov", "Marcus Cheung.mp4", "Maxwell Liu.mp4", "Natalie Tsang.mp4",
-        "Sek Ching Isabella Li.mp4", "Tin Yat Javen Hooi.mp4", "Tsz Yin Abby Siu.mp4",
-        "Zac Woo.mp4"
+        "Abby Yu" => "1jgf5FUlZct4B-skYCj_4Ay8ZrilA7yb9",
+        "Addison Ong" => "1IY7v6LUPE6HAvcAfdAZioazSaTT2HrM0",
+        "Adelle Wu" => "1wjgUwvmWFTFkY80zYOOzOj7slaYh39yI",
+        "Aison Chiu" => "1wfU4Dy-tX6RuHiyuHy5Q1vhskb37AXFf",
+        "Andreea Cheung" => "1o-gQyo9LGNufq10InRf1DBKnLq_1Jmf8",
+        "Annette Yu" => "1I6-6GMWimRy6XtN4beNDB_JjYznBJX1I",
+        "Brandon Kwok" => "1JJdDgjs4D42c4NnKXI2Ku7dqKqccCk0B",
+        "Charles Liu" => "1fSzbklx1FVPwWwy-xi-eNzVp4kQ-jYQP",
+        "Chloe Wong" => "1LnB7nsnmWnWC1OBRtToEFEZ0pfPquAJ7",
+        "Curtis Leung" => "1HO8QyEUM5SJvDu2koB6gdO79KIgFt9nQ",
+        "Curtis Wong" => "1FOVJlbMoN9WPnfbKDlYcgtVxnxQa_z4Y",
+        "Cyrus Lai" => "1hp6VIfWMq1zTmsnf7DPk7CTX2gAzl5pw",
+        "Elgar Lam" => "1TRNgBEd6iQeb1U7igcIuh14loxDg1MGO",
+        "Gwan Yu Raven Siu" => "1I32_xN4TpkCgjYoIj7T7IGr6onl_0RU4",
+        "Hei Yu Hailey Siu" => "1gpYdsEC0PR0EieG6DtCQjSREb2QnSweK",
+        "Jayden Heng" => "1bKTOCp8DpALFnIEeq6C42AlAEl0ESQSc",
+        "Marcus Cheung" => "1Sw8cl4cD-SF-r755Dwf4IdWnnT8j5MRv",
+        "Maxwell Liu" => "1pMBdzfvNhbLTKGV1JpVMy6itBj_qQyXl",
+        "Natalie Tsang" => "1T8vjmFJPrfPaV_n8OrAhGThuMhNf1uQA",
+        "Sek Ching Isabella Li" => "1ImgKWFEKdRfjy-7_mkzgrHRVmsAEjC4u",
+        "Tin Yat Javen Hooi" => "16V-wP3FFLclw3zYRF_ZkTkrhb2oRiRGL",
+        "Tsz Yin Abby Siu" => "1lNJMD8rNKpl-fZ6k4IIVhvkTC083tTSm",
+        "Zac Woo" => "1RLZWdZd22h7-6ADTe_uveEAqlKHTujJB"
     );
     
-    foreach($videos as $video) {
-        $name = pathinfo($video, PATHINFO_FILENAME);
-        echo "<li><a href='video.php?name=" . urlencode($name) . "'>" . htmlspecialchars($name) . "</a></li>";
+    foreach($videos as $name => $fileId) {
+        echo "<li><a href='video.php?name=" . urlencode($name) . "&id=" . urlencode($fileId) . "'>" . htmlspecialchars($name) . "</a></li>";
     }
     ?>
     </ul>
--- video.php
+++ video.php
@@ -1,30 +1,18 @@
 <?php
 $name = isset($_GET['name']) ? $_GET['name'] : '';
-$videoExtensions = array('.mp4', '.mov');
-$videoFile = '';
+$fileId = isset($_GET['id']) ? $_GET['id'] : '';
 
-foreach ($videoExtensions as $ext) {
-    if (file_exists("videos/" . $name . $ext)) {
-        $videoFile = "videos/" . $name . $ext;
-        break;
-    }
-}
-
-if (!$videoFile) {
+if (!$fileId) {
     die("Video not found");
 }
+
+$embedUrl = "https://drive.google.com/file/d/" . $fileId . "/preview";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-      rel="stylesheet"
-    />
     <title><?= htmlspecialchars($name) ?></title>
     <style>
         body, html {
@@ -37,21 +25,16 @@ if (!$videoFile) {
             align-items: center;
             background-color: #000;
             color: #fff;
-            font-family: "Montserrat", sans-serif;
+            font-family: Arial, sans-serif;
         }
         .video-container {
             width: 95vw;
             height: 85vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
         }
-        video {
+        iframe {
             width: 100%;
             height: 100%;
-            object-fit: contain;
-            max-width: 100%;
-            max-height: 100%;
+            border: none;
         }
         h1 {
             margin-bottom: 20px;
@@ -62,10 +45,7 @@ if (!$videoFile) {
 <body>
     <h1><?= htmlspecialchars($name) ?></h1>
     <div class="video-container">
-        <video controls autoplay>
-            <source src="<?= htmlspecialchars($videoFile) ?>" type="video/<?= pathinfo($videoFile, PATHINFO_EXTENSION) ?>">
-            Your browser does not support the video tag.
-        </video>
+        <iframe src="<?= htmlspecialchars($embedUrl) ?>" allowfullscreen></iframe>
     </div>
 </body>
 </html>
